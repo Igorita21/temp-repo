dfea6b9f1f5643e2759d049aa96ab8838cbd7a5c
Igor Amaral <igoruftm@gmail.com>
2022-12-18 03:18:25 -0300

    primeiro commit projeto do email apenas de estudos, não é o perfil profissional

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export const TextInput = ({searchValue, handleChange}) => {
+    return (
+        <input className='text-input'
+        onChange={handleChange}
+        value={searchValue}
+        placeholder="type your search"
+        type="search" /> 
+    );
+}
